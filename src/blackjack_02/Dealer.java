aa5deb31ab6ae6edb94ee02f3748023f63af8e4d
knightt022 <knight022@gannon.edu>
2020-02-12 15:42:35 -0500

    finished BlackJack game

--- src/blackjack_02/Dealer.java
+++ src/blackjack_02/Dealer.java
@@ -30,6 +30,13 @@ public class Dealer {
         initPlayers(num);
     }
     
+    public void playGame(){
+        this.dealOutOpeningHand();
+        this.playOutPlayerHands();
+        this.playOutDealerHand();
+        this.declareWinners();
+    }
+    
     public void dealOutOpeningHand(){
         for(int i = 0; i < 2; i++){
             for(Player currPlayer : myPlayers){
@@ -67,6 +74,36 @@ public class Dealer {
     
     public void declareWinners(){
         
+        for(int i = 0; i < myPlayers.length; i++){
+            Player currPlayer = myPlayers[i];
+            System.out.println(currPlayer.getName()+ "'s Hand");
+            currPlayer.getMyHand().printHand();
+            if(dealerHand.getScore() > 21 ||
+                    currPlayer.getMyHand().getScore() > 21){
+                if(currPlayer.getMyHand().getScore() > 21){
+                    System.out.println(currPlayer.getName() + "you're busted"
+                            + "....LOSER!!!!");
+                }else{
+                    System.out.println(currPlayer.getName() + " "
+                            + " the dealer busted...you got lucky..");
+                }
+            }else if(dealerHand.getScore()==21 ||
+                    dealerHand.getNumOfCards() > 4){
+                System.out.println(currPlayer.getName() + ""
+                        + " the dealer is too good for you....novists!!");
+            }else if(currPlayer.getMyHand().getNumOfCards() > 4){
+                System.out.println(currPlayer.getName() + "" 
+                        + "you win I guess sometimes the sun even shines "
+                        + "on a dogs ass");
+            }else if(currPlayer.getMyHand().getScore() > dealerHand.getScore()){
+                System.out.println(currPlayer.getName() + ""
+                        + "you win this hand... but there will be others");
+            }else{
+                System.out.println(currPlayer.getName());
+                
+                
+            }
+        }
     }
     
     private void initPlayers(int numOfPlayers){
--- src/blackjack_02/Hand.java
+++ src/blackjack_02/Hand.java
@@ -46,7 +46,8 @@ public class Hand {
     
     public void printHand(){
         for(int i = 0; i < numOfCards; i++){
-            System.out.println(myCards[i].RANK + " of " + myCards[i].SUIT);
+            System.out.println(myCards[i].RANK + " of " + myCards[i].SUIT + 
+                    "\n");
         }
     }
     
